4fc004ca965ba60edb2b28586d52eac9000aa5ea
Russell Power <power@cs.nyu.edu>
2012-11-21 13:41:05 -0500

    Try to automate finding of various required path variables.

--- cuda-convnet/build.sh
+++ cuda-convnet/build.sh
@@ -1,1 +1,32 @@
-build-rose.sh
+#!/bin/bash
+
+# Fill in these environment variables.
+# I have tested this code with CUDA 4.0, 4.1, and 4.2. 
+# Only use Fermi-generation cards. Older cards won't work.
+
+# Try to determine various configurations automatically from numpy/python-config
+./sysinfo.sh
+source /tmp/convnet-config
+
+echo Found ATLAS: ${ATLAS_LIB_PATH}
+echo Found NUMPY: ${NUMPY_INCLUDE_PATH}
+echo Found PYTHON_INCLUDE_PATH: ${PYTHON_INCLUDE_PATH}
+echo Found MPI_INCLUDE: ${MPI_INCLUDE}
+echo Found MPI_LINK: ${MPI_LINK}
+
+#export ATLAS_LIB_PATH=...
+#export NUMPY_INCLUDE_PATH=...
+#export PYTHON_INCLUDE_PATH=...
+#export MPI_INCLUDE=...
+#export MPI_LINK=...
+
+# CUDA toolkit installation directory.
+export CUDA_INSTALL_PATH=/home/power/pkg/cuda-4.2
+
+# CUDA SDK installation directory.
+export CUDA_SDK_PATH=/home/power/pkg/gpusdk
+
+
+
+make $*
+
